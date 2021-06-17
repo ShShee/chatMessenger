6013c5ab328607bcd1c13c6f6ac1e32b2e8a4d5e
Đặng Duy Bằng <81517068+DangDuyBang@users.noreply.github.com>
2021-06-17 22:09:05 +0700

    doi ten ben trong Main sau khi dang nhap

--- app/src/main/java/com/SE114PMCL/chatMessenger/MainActivity.java
+++ app/src/main/java/com/SE114PMCL/chatMessenger/MainActivity.java
@@ -17,8 +17,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
+import com.SE114PMCL.chatMessenger.Model.UserModel;
 import com.SE114PMCL.chatMessenger.R;
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -28,6 +37,11 @@ public class MainActivity extends AppCompatActivity{
     Toolbar toolbar;
     BottomNavigationView bottomNav;
 
+    CircleImageView image_chat;
+    TextView username_chat;
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +50,31 @@ public class MainActivity extends AppCompatActivity{
         bottomNav=findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         toolbar=findViewById(R.id.toolbarUsername);
+
+        image_chat = (CircleImageView) findViewById(R.id.chatImage);
+        username_chat = (TextView) findViewById(R.id.chatName);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                username_chat.setText(userModel.getUsername());
+                if(userModel.getImageURL().equals("default")){
+                    image_chat.setImageResource(R.mipmap.ic_launcher);
+                }else{
+                    Glide.with(MainActivity.this).load(userModel.getImageURL()).into(image_chat);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
         setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
